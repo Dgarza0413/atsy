9cee76a6b26958d3fad6a3afb91ccabbbf681aa6
David Garza <dgarza0413@gmail.com>
2020-02-09 17:31:17 -0600

    added delete route for id item

--- components/Product/AddProductToCart.js
+++ components/Product/AddProductToCart.js
@@ -1,5 +1,17 @@
+import { input, Input, Icon } from 'semantic-ui-react';
+
 function AddProductToCart() {
-  return <>AddProductToCart</>;
+  return <Input
+    type='number'
+    min='1'
+    placeholder="Quantity"
+    value={1}
+    action={{
+      color: 'orange',
+      content: "Add to Cart",
+      icon: "plus cart"
+    }}
+  />;
 }
 
 export default AddProductToCart;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import ProductList from '../components/Index/ProductList.js'
+import baseUrl from '../utils/baseUrl';
 
 function Home({ products }) {
   return <ProductList products={products} />;
@@ -8,7 +9,7 @@ function Home({ products }) {
 
 Home.getInitialProps = async () => {
   //fetch data on server
-  const url = 'http://localhost:3000/api/products';
+  const url = `${baseUrl}/api/products`;
   const response = await axios.get(url);
   return { products: response.data }
   //return response data as an object
