49e34f615638497b7c5d70e9d5c0a56d7fd04653
David Garza <dgarza0413@gmail.com>
2020-02-13 17:55:57 -0600

    handle invalid token

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import App from 'next/app';
 import Layout from '../components/_App/Layout';
-import { parseCookies } from 'nookies';
+import { parseCookies, destroyCookie } from 'nookies';
 import { redirectUser } from '../utils/auth';
 import baseUrl from '../utils/baseUrl';
 import axios from 'axios';
@@ -30,6 +30,11 @@ class MyApp extends App {
         pageProps.user = user
       } catch (error) {
         console.error("Error getting current user", error)
+        // throw out invalid token
+        destroyCookie(ctx, "token");
+        // redirect to login
+        redirectUser(ctx, "/login");
+
       }
     }
 
