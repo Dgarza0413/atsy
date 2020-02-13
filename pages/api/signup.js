9dd3bb64c9204c10cb90e9b69fc3b55c5bcdb0e2
David Garza <dgarza0413@gmail.com>
2020-02-13 16:26:56 -0600

    signup with cart and cart creation model

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,5 +1,6 @@
 import connectDb from '../../utils/connectDb';
 import User from '../../models/User'
+import Cart from '../../models/Cart';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import isEmail from 'validator/lib/isEmail';
@@ -30,6 +31,9 @@ export default async (req, res) => {
         const newUser = await new User({ name, email, password: hash }).save()
 
         console.log({ newUser })
+        //create care for new user
+        await new Cart({ user: newUser._id }).save()
+
         // create token for the new user
         const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
             expiresIn: '7d'
