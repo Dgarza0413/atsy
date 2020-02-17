e006a807f74e7e3a4e8f98da769a9274db1cc1ad
David Garza <dgarza0413@gmail.com>
2020-02-16 20:35:32 -0600

    baseUrl change

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,5 @@
 const baseUrl = process.env.NODE_ENV === 'production'
-    ? 'https://deployment-url.now.sh'
+    ? 'https://blooming-lowlands-48211.herokuapp.com'
     : 'http://localhost:3000'
-
 
 export default baseUrl;
