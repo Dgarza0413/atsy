c387de8f58dde4482d0f9e4702f6138ba29fe692
David Garza <dgarza0413@gmail.com>
2021-01-06 17:10:58 -0600

    changed title to new one

--- components/_App/Header.js
+++ components/_App/Header.js
@@ -29,7 +29,7 @@ function Header({ user }) {
               src="/static/logo.svg"
               style={{ marginRight: '1em' }}
             />
-            React Reserve
+            Atsy
           </Menu.Item>
         </Link>
         <Link href="/cart">
